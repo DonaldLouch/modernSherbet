051e45dccea4ba1c6737eb46773e38924f563529
Donald Louch <donaldlouch@outlook.com>
2018-07-23 10:59:02 -0700

    Add files via upload

---
+++ scripts/js/toggle.js
@@ -0,0 +1,9 @@
+console.log("toggle.js is working!")
+
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
