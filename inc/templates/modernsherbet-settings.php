a171056b335d692d6ff855e74a190cba71161f55
Donald Louch <donaldlouch@outlook.com>
2018-07-23 20:48:47 -0700

    New ID: modernsherbetSettings

--- inc/templates/modernsherbet-settings.php
+++ inc/templates/modernsherbet-settings.php
@@ -1,8 +1,10 @@
-<h1>modernSherbet Settings</h1>
-<h3>Installed Version 1.1.2</h3>
-<?php settings_errors(); ?>
-  <form method="post" action="options.php">
-    <?php settings_fields( 'modernsherbet-settings' ); ?>
-    <?php do_settings_sections( 'modernsherbet_settings' ); ?>
-    <?php submit_button( 'Save Settings' );  ?>
-  </form>
+<section id="modernsherbetSettings">
+  <h1>modernSherbet Settings</h1>
+  <h3>Installed Version 1.1.2</h3>
+  <?php settings_errors(); ?>
+    <form method="post" action="options.php">
+      <?php settings_fields( 'modernsherbet-settings' ); ?>
+      <?php do_settings_sections( 'modernsherbet_settings' ); ?>
+      <?php submit_button( 'Save Settings' );  ?>
+    </form>
+</section>
