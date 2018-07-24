d578696b1f445c5ea246f4a89ed46f037efd7034
Donald Louch <donaldlouch@outlook.com>
2018-07-23 20:41:48 -0700

    Updated version number!

--- inc/templates/modernsherbet-settings.php
+++ inc/templates/modernsherbet-settings.php
@@ -1,5 +1,5 @@
 <h1>modernSherbet Settings</h1>
-<h3>Installed Version 1.1.1</h3>
+<h3>Installed Version 1.1.2</h3>
 <?php settings_errors(); ?>
   <form method="post" action="options.php">
     <?php settings_fields( 'modernsherbet-settings' ); ?>
