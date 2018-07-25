25d11e6b53ffee959f609782d6f95fb22ffcd3f3
Donald Louch <donaldlouch@outlook.com>
2018-07-24 20:29:31 -0700

    Cleaner code

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -1,21 +1,19 @@
 <?php
-/* remove version string from js and css */
-function sherbet_remove_wp_version_strings( $src ) {
-	
-	global $wp_version;
-	parse_str( parse_url($src, PHP_URL_QUERY), $query );
-	if ( !empty( $query['ver'] ) && $query['ver'] === $wp_version ) {
-		$src = remove_query_arg( 'ver', $src );
+	/* remove version string from js and css */
+	function sherbet_remove_wp_version_strings( $src ) {
+		global $wp_version;
+			parse_str( parse_url($src, PHP_URL_QUERY), $query );
+			if ( !empty( $query['ver'] ) && $query['ver'] === $wp_version ) {
+				$src = remove_query_arg( 'ver', $src );
+			}
+		return $src;
 	}
-	return $src;
-	
-}
-add_filter( 'script_loader_src', 'sherbet_remove_wp_version_strings' );
-add_filter( 'style_loader_src', 'sherbet_remove_wp_version_strings' );
+	add_filter( 'script_loader_src', 'sherbet_remove_wp_version_strings' );
+	add_filter( 'style_loader_src', 'sherbet_remove_wp_version_strings' );
 
-/* remove metatag generator from header */
-function sherbet_remove_meta_version() {
-	return '';
-}
-add_filter( 'the_generator', 'sherbet_remove_meta_version' );
+	/* remove metatag generator from header */
+	function sherbet_remove_meta_version() {
+		return '';
+	}
+	add_filter( 'the_generator', 'sherbet_remove_meta_version' );
 ?>
